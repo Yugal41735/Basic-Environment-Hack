ef29dfee2a23ce1f47de1ddb6577448f430eb4a4
Yugal Agarwal <yugal1434@gmail.com>
2021-07-02 19:19:45 +0530

    Second Commit

---
+++ environment.py
@@ -0,0 +1,9 @@
+print("Measuring temperature from nearby surroundings!!")
+s = int(input())
+
+if s > 30:
+    print("Temp is very hot!!!")
+    print("More trees to be planted in this area!!")
+else:
+    print("The temp in the surrounding is normal :)")
+    print("Make sure to take care of the surrounding!! :)")
